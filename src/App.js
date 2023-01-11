ab42464f2df09fd85a8186c5fa8fd68c59478a8d
franky <itskchills11@gmail.com>
2023-01-11 13:37:24 +0530

    sidebar added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <Topbar />
       <div className="container">
         <Sidebar />
-        {/* <div className="others">Other Pages</div> */}
+        <div className="others">Other Pages</div>
       </div>
     </div>
   );
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,64 @@
 import HomeIcon from "@mui/icons-material/Home";
+import AnalyticsIcon from "@mui/icons-material/Analytics";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import PeopleOutlineIcon from "@mui/icons-material/PeopleOutline";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import PaidIcon from "@mui/icons-material/Paid";
+import MessageIcon from "@mui/icons-material/Message";
+import EmailIcon from "@mui/icons-material/Email";
+import FeedbackIcon from "@mui/icons-material/Feedback";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
-          <h3 className="sidebardTitle">Dashboard</h3>
+          <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <HomeIcon fontSize="large" className="sbicon" /> <b>Home</b>
+            </li>
             <li className="sidebarListItem">
-              <HomeIcon fontSize="large" /> Home
+              <AnalyticsIcon fontSize="large" className="sbicon" />{" "}
+              <b> Analytics</b>
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon fontSize="large" className="sbicon" />
+              <b> Sale</b>
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <PeopleOutlineIcon fontSize="large" className="sbicon" />
+              <b>Users</b>
+            </li>
+            <li className="sidebarListItem">
+              <LocalMallIcon fontSize="large" className="sbicon" />
+              <b>Products</b>
+            </li>
+            <li className="sidebarListItem">
+              <PaidIcon fontSize="large" className="sbicon" />
+              <b>Transcations</b>
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <MessageIcon fontSize="large" className="sbicon" />
+              <b>Messages</b>
+            </li>
+            <li className="sidebarListItem">
+              <EmailIcon fontSize="large" className="sbicon" />
+              <b>Mail</b>
+            </li>
+            <li className="sidebarListItem">
+              <FeedbackIcon fontSize="large" className="sbicon" />
+              <b>Feedbacks</b>
             </li>
           </ul>
         </div>
